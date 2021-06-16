92978c71b3d69450751c949d057f9edcdcb498e1
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-16 16:44:18 -0300

    Add project files.

---
+++ QuadradodeParesURI1073/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace QuadradodeParesURI1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recebe o número informado
+            double n = double.Parse(Console.ReadLine());            
+            
+            for (int i = 2; i <= n; i++)
+            {
+                //Verifica os números pares e executa o cálculo
+                if (i % 2 == 0)
+                {
+                    double resultado = 0;
+                    resultado = Math.Pow(i, 2);
+                    Console.WriteLine(i + "^2" + " = " + resultado);
+                }
+            }
+        }
+    }
+}
